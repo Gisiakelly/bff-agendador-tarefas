8152d1a75a1134d1e6711a987385ad45e13ba71f
Gísia <gisia.luan@gmail.com>
2026-04-10 16:05:43 -0300

    adicionado Cron

--- bff-agendador-tarefas/src/main/java/com/gisia/bffagendadortarefas/infrastructure/client/EmailClient.java
+++ bff-agendador-tarefas/src/main/java/com/gisia/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -14,5 +14,6 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 }
